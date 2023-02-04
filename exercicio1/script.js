3fbff28426fbcc1f1e71cdc0b1d3371e89a47819
Henrique Horbilon <henriquehorbilon@gmail.com>
2023-02-04 12:29:07 -0300

    3 exercicios Operadores

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+let pergunta1 = prompt('Digite um número par:')
+let calc = pergunta1 % 2
+console.log(`Resto da divisão: ${calc}`)
+
+// c) Todos possuem resto 0, pois são pares.
+// d) Apresenta resto 1, pois é ímpar.
